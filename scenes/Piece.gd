266094e83c48d47d40ccc1a92eaaf968c3ea425c
kido321 <kiduswork2@gmail.com>
2022-12-06 20:27:13 -0600

    tetris

--- scenes/Piece.gd
+++ scenes/Piece.gd
@@ -11,65 +11,65 @@ enum Rotations { ZERO, NINETY, ONE_EIGTHY, TWO_SEVENTY }
 const BLOCK_SIZE = 18
 
 const SHAPE_COLOR = [
-    BlockColors.Cyan,     # I
-    BlockColors.Red,      # Z
-    BlockColors.Green,    # S
-    BlockColors.Purple,   # T
-    BlockColors.Orange,   # L
-    BlockColors.Blue,     # J
-    BlockColors.Yellow,   # O
+	BlockColors.Cyan,     # I
+	BlockColors.Red,      # Z
+	BlockColors.Green,    # S
+	BlockColors.Purple,   # T
+	BlockColors.Orange,   # L
+	BlockColors.Blue,     # J
+	BlockColors.Yellow,   # O
 ]
 
 const SHAPES = [
-    # I
-    [
-        Vector2(-1.5,-0.5),
-        Vector2(-0.5,-0.5),
-        Vector2(0.5,-0.5),
-        Vector2(1.5,-0.5),
-    ],
-    # Z
-    [
-        Vector2(-0.5,-0.5),
-        Vector2(0.5,-0.5),
-        Vector2(0.5,0.5),
-        Vector2(1.5,0.5),
-    ],
-    # S
-    [
-        Vector2(0.5,-0.5),
-        Vector2(1.5,-0.5),
-        Vector2(-0.5,0.5),
-        Vector2(0.5,0.5),
-    ],
-    # T
-    [
-        Vector2(-0.5,-0.5),
-        Vector2(0.5,-0.5),
-        Vector2(1.5,-0.5),
-        Vector2(0.5,0.5),
-    ],
-    # L
-    [
-        Vector2(-0.5,-0.5),
-        Vector2(0.5,-0.5),
-        Vector2(1.5,-0.5),
-        Vector2(-0.5,0.5),
-    ],
-    # J
-    [
-        Vector2(1.5,0.5),
-        Vector2(-0.5,-0.5),
-        Vector2(0.5,-0.5),
-        Vector2(1.5,-0.5),
-    ],
-    # O
-    [
-        Vector2(-0.5,-0.5),
-        Vector2(0.5,-0.5),
-        Vector2(-0.5,0.5),
-        Vector2(0.5,0.5),
-    ],
+	# I
+	[
+		Vector2(-1.5,-0.5),
+		Vector2(-0.5,-0.5),
+		Vector2(0.5,-0.5),
+		Vector2(1.5,-0.5),
+	],
+	# Z
+	[
+		Vector2(-0.5,-0.5),
+		Vector2(0.5,-0.5),
+		Vector2(0.5,0.5),
+		Vector2(1.5,0.5),
+	],
+	# S
+	[
+		Vector2(0.5,-0.5),
+		Vector2(1.5,-0.5),
+		Vector2(-0.5,0.5),
+		Vector2(0.5,0.5),
+	],
+	# T
+	[
+		Vector2(-0.5,-0.5),
+		Vector2(0.5,-0.5),
+		Vector2(1.5,-0.5),
+		Vector2(0.5,0.5),
+	],
+	# L
+	[
+		Vector2(-0.5,-0.5),
+		Vector2(0.5,-0.5),
+		Vector2(1.5,-0.5),
+		Vector2(-0.5,0.5),
+	],
+	# J
+	[
+		Vector2(1.5,0.5),
+		Vector2(-0.5,-0.5),
+		Vector2(0.5,-0.5),
+		Vector2(1.5,-0.5),
+	],
+	# O
+	[
+		Vector2(-0.5,-0.5),
+		Vector2(0.5,-0.5),
+		Vector2(-0.5,0.5),
+		Vector2(0.5,0.5),
+	],
 ]
 
 export(bool) var static_mode = false
@@ -98,109 +98,109 @@ var game = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    self.blocks = [
-        $Block0,
-        $Block1,
-        $Block2,
-        $Block3,
-    ]
-    self.draw_shape(self.shape)
+	self.blocks = [
+		$Block0,
+		$Block1,
+		$Block2,
+		$Block3,
+	]
+	self.draw_shape(self.shape)
 
-    if !self.static_mode and !Engine.is_editor_hint():
-        yield(fall(), "completed")
+	if !self.static_mode and !Engine.is_editor_hint():
+		yield(fall(), "completed")
 
 
 func fall():
-    while !self.stopped:
-        if game != null and !self.get_tree().paused:
-            if game.is_valid_position(self, Vector2(0,-1)):
-                self.position.y += BLOCK_SIZE
-                if Input.is_action_pressed("down"):
-                    self.emit_signal("piece_is_soft_dropped", self)
-            elif self.lock_timer.is_stopped():
-                self.pre_lock_fx.play()
-                self.lock_timer.start(self.lock_delay)
+	while !self.stopped:
+		if game != null and !self.get_tree().paused:
+			if game.is_valid_position(self, Vector2(0,-1)):
+				self.position.y += BLOCK_SIZE
+				if Input.is_action_pressed("down"):
+					self.emit_signal("piece_is_soft_dropped", self)
+			elif self.lock_timer.is_stopped():
+				self.pre_lock_fx.play()
+				self.lock_timer.start(self.lock_delay)
 
-        yield(wait_time(), "completed")
+		yield(wait_time(), "completed")
 
 func lock():
-    if !game.is_valid_position(self, Vector2(0,-1)):
-        self.lock_fx.play()
-        self.emit_signal("piece_have_fallen", self)
-        self.lock_time = 0
-        self.stopped = true
-    else:
-        self.position.y += BLOCK_SIZE
-        self.lock_timer.start(self.lock_delay)
+	if !game.is_valid_position(self, Vector2(0,-1)):
+		self.lock_fx.play()
+		self.emit_signal("piece_have_fallen", self)
+		self.lock_time = 0
+		self.stopped = true
+	else:
+		self.position.y += BLOCK_SIZE
+		self.lock_timer.start(self.lock_delay)
 
 func wait_time():
-    var time = 0
-    yield(get_tree(), "idle_frame")
-    while time < self.time:
-        time += self.get_process_delta_time()
-        yield(get_tree(), "idle_frame")
+	var time = 0
+	yield(get_tree(), "idle_frame")
+	while time < self.time:
+		time += self.get_process_delta_time()
+		yield(get_tree(), "idle_frame")
 
 func _physics_process(delta):
-    if self.static_mode or Engine.is_editor_hint():
-        return
+	if self.static_mode or Engine.is_editor_hint():
+		return
 
-    if !self.stopped:
+	if !self.stopped:
 
-        if Input.is_action_pressed("down"):
-            self.time = BLOCK_SIZE / 360.0
-        else:
-            self.time = self.level_time
+		if Input.is_action_pressed("down"):
+			self.time = BLOCK_SIZE / 360.0
+		else:
+			self.time = self.level_time-0.85
 
-        if Input.is_action_just_pressed("rotate_right"):
-            self.rotationPosition = self.rotationPosition + 1 % 4
-            if !self.game.is_valid_position(self):
-                self.rotationPosition = self.rotationPosition - 1 % 4
+		if Input.is_action_just_pressed("rotate_right"):
+			self.rotationPosition = self.rotationPosition + 1 % 4
+			if !self.game.is_valid_position(self):
+				self.rotationPosition = self.rotationPosition - 1 % 4
 
-        if Input.is_action_just_pressed("rotate_left"):
-            self.rotationPosition = self.rotationPosition - 1 % 4
-            if !self.game.is_valid_position(self):
-                self.rotationPosition = self.rotationPosition + 1 % 4
+		if Input.is_action_just_pressed("rotate_left"):
+			self.rotationPosition = self.rotationPosition - 1 % 4
+			if !self.game.is_valid_position(self):
+				self.rotationPosition = self.rotationPosition + 1 % 4
 
-        if Input.is_action_just_pressed("left"):
-            if game.is_valid_position(self, Vector2(-1,0)):
-                self.position.x -= BLOCK_SIZE
+		if Input.is_action_just_pressed("left"):
+			if game.is_valid_position(self, Vector2(-1,0)):
+				self.position.x -= BLOCK_SIZE
 
-        if Input.is_action_just_pressed("right"):
-            if game.is_valid_position(self, Vector2(1,0)):
-                self.position.x += BLOCK_SIZE
+		if Input.is_action_just_pressed("right"):
+			if game.is_valid_position(self, Vector2(1,0)):
+				self.position.x += BLOCK_SIZE
 
 
 func draw_shape(shape):
-    if self.blocks.empty():
-        return
+	if self.blocks.empty():
+		return
 
-    for i in range(self.blocks.size()):
-        self.blocks[i].color = SHAPE_COLOR[shape]
-        self.blocks[i].position = SHAPES[shape][i] * BLOCK_SIZE
+	for i in range(self.blocks.size()):
+		self.blocks[i].color = SHAPE_COLOR[shape]
+		self.blocks[i].position = SHAPES[shape][i] * BLOCK_SIZE
 
 
 func _set_rotation(new_rotation):
-    rotationPosition = new_rotation
+	rotationPosition = new_rotation
 
-    if self.blocks.empty():
-        return
+	if self.blocks.empty():
+		return
 
-    for i in range(self.blocks.size()):
-        self.blocks[i].rotation = deg2rad(-90 * rotationPosition)
+	for i in range(self.blocks.size()):
+		self.blocks[i].rotation = deg2rad(-90 * rotationPosition)
 
-    self.rotation = deg2rad(90 * rotationPosition)
+	self.rotation = deg2rad(90 * rotationPosition)
 
 
 func get_time(level:int):
-    return pow((0.8 - ((level - 1) * 0.007)), level - 1)
+	return pow((0.8 - ((level - 1) * 0.007)), level - 1)
 
 
 func set_level(level:int):
-    self.level_time = get_time(level)
-    self.velocity = Vector2(0, BLOCK_SIZE / self.level_time)
-    # Time = (0.8-((Level-1)*0.007))^(Level-1)
+	self.level_time = get_time(level)
+	self.velocity = Vector2(0, BLOCK_SIZE / self.level_time)
+	# Time = (0.8-((Level-1)*0.007))^(Level-1)
 
 
 func _set_shape(new_shape: int):
-    shape = new_shape
-    self.draw_shape(self.shape)
+	shape = new_shape
+	self.draw_shape(self.shape)
